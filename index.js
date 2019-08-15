711c330c92737ca83c638e85bfd2107054bc20d8
DimasYudhistira91 <dimasmillaty@gmail.com>
2019-08-15 20:11:31 +0700

    learn about async and await

--- index.js
+++ index.js
@@ -162,3 +162,63 @@ function getCommits(repo, Callbacks) {
     }, 2000);
   });
 }
+
+
+// _____________________________________________________________\\
+
+// ASYNC & AWAIT
+
+console.log('Before');
+
+// Promise-based approach
+// getUser(1)
+//   .then(user => getRepositories(user.gitHubUsername))
+//   .then(repos => getCommits(repos[0]))
+//   .then(commits => console.log('Commit:', commits))
+//   .catch(err => console.log('Error', err.message));
+
+
+// Async & Await approach
+async function displayCommits() {
+  try {
+    const user = await getUser(1);
+    const repos = await getRepositories(user.gitHubUsername);
+    const commits = await getCommits(repos[0]);
+    console.log(commits);
+  }
+  catch (err) {
+    console.log('Error', err.message);
+  }
+}
+displayCommits();
+
+console.log('After');
+
+
+function getUser(id) {
+  return new Promise((resolve, reject) => {     // gunakan promises
+    setTimeout(() => {
+      console.log('Reading a user from a database...');
+      resolve({id: id, gitHubUsername: 'dimas'});
+    }, 2000);
+  });
+}
+
+function getRepositories(username) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Calling GitHub API...');
+      // resolve(['repo1', 'repo2', 'repo3']);
+      reject(new Error('Could not get the repos.'))
+    }, 2000);
+  });
+}
+
+function getCommits(repo, Callbacks) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Calling GitHub API...');
+      resolve(['commit']);
+    }, 2000);
+  });
+}
